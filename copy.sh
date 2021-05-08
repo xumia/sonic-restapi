2d065e33d848d403a39a832e9ecc7d0a2e7d3718
xumia <xumia@microsoft.com>
2021-05-08 09:05:45 +0000

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -6,6 +6,7 @@ cp $SDIR/libhiredis-dev_0.14.0-3~bpo9+1_amd64.deb debs
 cp $SDIR/libnl-3-200_3.5.0-1_amd64.deb debs
 cp $SDIR/libnl-genl-3-200_3.5.0-1_amd64.deb debs
 cp $SDIR/libnl-route-3-200_3.5.0-1_amd64.deb debs
+cp $SDIR/libnl-route-3-dev_3.5.0-1_amd64.deb debs
 cp $SDIR/libnl-nf-3-200_3.5.0-1_amd64.deb debs
 cp $SDIR/libnl-nf-3-dev_3.5.0-1_amd64.deb debs
 cp $SDIR/libswsscommon_1.0.0_amd64.deb debs
