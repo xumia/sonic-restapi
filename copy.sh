5c68e6be3720f4536559e09c77e4dffc3e2b4427
xumia <xumia@microsoft.com>
2021-05-08 10:05:23 +0000

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -4,7 +4,9 @@ SDIR=../target/debs/buster
 cp $SDIR/libhiredis0.14_0.14.0-3~bpo9+1_amd64.deb debs
 cp $SDIR/libhiredis-dev_0.14.0-3~bpo9+1_amd64.deb debs
 cp $SDIR/libnl-3-200_3.5.0-1_amd64.deb debs
+cp $SDIR/libnl-3-dev_3.5.0-1_amd64.deb debs
 cp $SDIR/libnl-genl-3-200_3.5.0-1_amd64.deb debs
+cp $SDIR/libnl-genl-3-dev_3.5.0-1_amd64.deb debs
 cp $SDIR/libnl-route-3-200_3.5.0-1_amd64.deb debs
 cp $SDIR/libnl-route-3-dev_3.5.0-1_amd64.deb debs
 cp $SDIR/libnl-nf-3-200_3.5.0-1_amd64.deb debs
